b6ee7d5f3cb6e3127f190a1bd94df224642750fc
kergrau <kergrau@hotmail.com>
2019-05-14 12:41:35 -0500

    Se reubico metodo de dni a id del controlador al modelo railscar

--- app/models/railcar.rb
+++ app/models/railcar.rb
@@ -1,7 +1,18 @@
 class Railcar < ApplicationRecord
+  before_validation :dni_id, on: :create
   validates :placa, uniqueness: true
   validates :people_id, presence: true
   after_validation :mayus_minus, :on => [:create, :update]
+
+  def dni_id 
+    people_id = Person.select(Person.arel_table[:id]).
+    where(Person.arel_table[:dni].eq(self.people_id))
+    if people_id.blank?
+      self.people_id =''
+    else
+      self.people_id = people_id[0]['id']
+    end
+  end
 
   private
 
