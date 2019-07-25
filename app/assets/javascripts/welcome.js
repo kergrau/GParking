22155f79e67c53a91de908c891c6a53ef586e777
kergrau <kergrau@hotmail.com>
2019-07-25 16:05:34 -0500

    add mapbox map

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -31,12 +31,11 @@ function createMap() {
 }
 
 function allOne() {
-    var map = L.map('map').setView([0, 0], 2);
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 18,
-        id: 'mapbox.streets',
-        accessToken: 'pk.eyJ1Ijoic3luYXBrZyIsImEiOiJjanlhcHZ5c3EwMTU1M21yd2YwZTNzdWkzIn0.YteazFgKGN-GPb2yS2tPWA'
-    }).addTo(map);
-    L.Control.geocoder().addTo(map);
+    mapboxgl.accessToken = 'pk.eyJ1Ijoic3luYXBrZyIsImEiOiJjanlhcHZ5c3EwMTU1M21yd2YwZTNzdWkzIn0.YteazFgKGN-GPb2yS2tPWA';
+    var map = new mapboxgl.Map({
+        container: 'map', // container id
+        style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+        center: [-74.50, 40], // starting position [lng, lat]
+        zoom: 9 // starting zoom
+    });
 }
