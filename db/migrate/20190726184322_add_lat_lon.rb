cf09422d52e1031f2df2d24b0dda03064cbd96f4
kergrau <kergrau@hotmail.com>
2019-10-31 12:16:43 -0500

    Correccion de vistas para parqueadero

--- db/migrate/20190726184322_add_lat_lon.rb
+++ db/migrate/20190726184322_add_lat_lon.rb
@@ -1,7 +1,7 @@
 class AddLatLon < ActiveRecord::Migration[5.2]
   def change
-    add_column :parkings, :par_latitude, :string, limit: 15
-    add_column :parkings, :par_longitude, :string, limit: 15
+    #add_column :parkings, :par_latitude, :string, limit: 15
+    #add_column :parkings, :par_longitude, :string, limit: 15
     add_column :spaces, :parking_id, :integer
   end
 end
